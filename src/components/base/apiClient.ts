eac5d51849ea759985dd35db786ded824b8d2928
AntonZhuravskiy <petropak2014@yandex.ru>
2025-09-08 11:07:34 +0300

    Обновленные запросы api

--- src/components/base/apiClient.ts
+++ src/components/base/apiClient.ts
@@ -1,16 +1,31 @@
-import { IProduct, IOrder } from '../../types';
-import { Api } from './api';
+import { Api, ApiListResponse } from './api';
+import { IApiClient, IProduct, IOrderRequest, IOrderResponse } from '../../types';
 
-export class ApiClient extends Api {
-	constructor(baseUrl: string) {
-		super(baseUrl);
+export class ApiClient extends Api implements IApiClient {
+	protected readonly cdn: string;
+
+	constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+		super(baseUrl, options);
+		this.cdn = cdn;
 	}
 
 	fetchProducts(): Promise<IProduct[]> {
-		return this.get<IProduct[]>('/products');
+		return this.get('/product').then((data: ApiListResponse<IProduct>) =>
+			data.items.map((item) => ({
+				...item,
+				image: this.cdn + item.image,
+			}))
+		);
 	}
 
-	createOrder(order: IOrder): Promise<IOrder> {
-		return this.post<IOrder>('/order', order);
+	fetchProduct(id: string): Promise<IProduct> {
+		return this.get(`/product/${id}`).then((item: IProduct) => ({
+			...item,
+			image: this.cdn + item.image,
+		}));
+	}
+
+	createOrder(order: IOrderRequest): Promise<IOrderResponse> {
+		return this.post('/order', order).then((data: IOrderResponse) => data);
 	}
 }
--- src/components/base/events.ts
+++ src/components/base/events.ts
@@ -1,3 +1,5 @@
+// Хорошая практика даже простые типы выносить в алиасы
+// Зато когда захотите поменять это достаточно сделать в одном месте
 type EventName = string | RegExp;
 type Subscriber = Function;
 type EmitterEvent = {
@@ -8,8 +10,14 @@ type EmitterEvent = {
 export interface IEvents {
     on<T extends object>(event: EventName, callback: (data: T) => void): void;
     emit<T extends object>(event: string, data?: T): void;
+    trigger<T extends object>(event: string, context?: Partial<T>): (data: T) => void;
 }
 
+/**
+ * Брокер событий, классическая реализация
+ * В расширенных вариантах есть возможность подписаться на все события
+ * или слушать события по шаблону например
+ */
 export class EventEmitter implements IEvents {
     _events: Map<EventName, Set<Subscriber>>;
 
@@ -17,6 +25,9 @@ export class EventEmitter implements IEvents {
         this._events = new Map<EventName, Set<Subscriber>>();
     }
 
+    /**
+     * Установить обработчик на событие
+     */
     on<T extends object>(eventName: EventName, callback: (event: T) => void) {
         if (!this._events.has(eventName)) {
             this._events.set(eventName, new Set<Subscriber>());
@@ -24,6 +35,9 @@ export class EventEmitter implements IEvents {
         this._events.get(eventName)?.add(callback);
     }
 
+    /**
+     * Снять обработчик с события
+     */
     off(eventName: EventName, callback: Subscriber) {
         if (this._events.has(eventName)) {
             this._events.get(eventName)!.delete(callback);
@@ -33,6 +47,9 @@ export class EventEmitter implements IEvents {
         }
     }
 
+    /**
+     * Инициировать событие с данными
+     */
     emit<T extends object>(eventName: string, data?: T) {
         this._events.forEach((subscribers, name) => {
             if (name === '*') subscribers.forEach(callback => callback({
@@ -45,14 +62,23 @@ export class EventEmitter implements IEvents {
         });
     }
 
+    /**
+     * Слушать все события
+     */
     onAll(callback: (event: EmitterEvent) => void) {
         this.on("*", callback);
     }
 
+    /**
+     * Сбросить все обработчики
+     */
     offAll() {
         this._events = new Map<string, Set<Subscriber>>();
     }
 
+    /**
+     * Сделать коллбек триггер, генерирующий событие при вызове
+     */
     trigger<T extends object>(eventName: string, context?: Partial<T>) {
         return (event: object = {}) => {
             this.emit(eventName, {
@@ -62,3 +88,4 @@ export class EventEmitter implements IEvents {
         };
     }
 }
+
